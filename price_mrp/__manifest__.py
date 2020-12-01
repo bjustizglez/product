bfaeb11cabd83378b142baf31a6bc1951a9dcf95
Bernardo Justiz Gonzalez <bernardo.justiz@desoft.cu>
2020-11-30 22:22:27 -0500

    Update

---
+++ price_mrp/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Part of BJustiz. See LICENSE file for full copyright and licensing details.*-
+
+{
+    'name': 'Sale Prices - Manufacturing',
+    'category': 'Manufacturing/Manufacturing',
+    'summary': 'Sale Prices from Manufacturing bill of material',
+    'author': 'Xookweb',
+    'version': '1.0',
+    'website': 'https://github.com/bjustizglez/mrp',
+    'description': "Sale Prices from Manufacturing",
+    'license': 'OPL-1',
+    'currency': 'EUR',
+    'price': 15.0,
+    'depends': ['mrp'],
+    'data': [
+        'views/product_views.xml',
+    ],
+    'installable': True,
+}
